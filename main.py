8400bef72ed3a6a30e11677e0e9dc6f9400c0916
MoiseevDI <moiseev.david@yandex.ru>
2023-04-26 14:12:56 +0300

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/3060stats")
 async def root():
-    raw = requests.get("https://minerstat/v2/hardware?type=gpu")
+    raw = requests.get("https://api.minerstat.com/v2/hardware?type=gpu")
     stat_3060 = {}
     for item in raw.json():
         if item["url"] == "nvidia-rtx-3060":
