1c1b26b408bfae2a01eb4944217f5f0f5ec12f00
MoiseevDI <moiseev.david@yandex.ru>
2023-04-27 13:25:48 +0300

    test logs added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 import requests
+from pprint import pprint
 
 app = FastAPI()
 
 @app.get("/newssa")
 async def root():
     raw = requests.get("http://logic/v1alpha/newsanalyses")
+    pprint(raw.json())
     return raw.json()
